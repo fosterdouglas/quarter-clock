614e677a5f0d37006d560b1fcb1a9210d837000a
Foster Douglas <foster@sandcastle.co>
2021-09-28 02:36:58 -0700

    Add files via upload

--- quarter-clock.py
+++ quarter-clock.py
@@ -18,23 +18,30 @@ rtc=rv3028_rtc.RV3028(0x52, i2c, "LSM")
 currentSecond = rtc.get_seconds()
 currentMinute = rtc.get_minutes()
 currentHour = rtc.get_hours()
+currentDate = rtc.get_date()
+currentMonth = rtc.get_month()
 
 pulse = True
-quarterOffsets = [8,10,12,14]
+quarterOffsets = [0, 8, 10, 12, 14]
 
+#TODO: reduce as many of these as possible
 global currentQuarter
 global brt #brightness
-global hr #hour
-global shift #color shift
+#global hr #hour
 global hr12mode #24 or 12h clock
 global hrdelta #difference in hours from system clock
+global theme_hue
+global theme_sat
+global palette_list
+global current_palette
+global dst
 
 currentQuarter = 0
 brt = 0.8 #start brightness at one level down
 hr = currentHour
-shift = 0
 hr12mode = False
 hrdelta = 0
+dst = 0
 
 # using these instead of picounicorn's built in functions to be able to access IRQs
 button_a = Pin(12, Pin.IN, Pin.PULL_UP)
@@ -42,15 +49,25 @@ button_b = Pin(13, Pin.IN, Pin.PULL_UP)
 button_x = Pin(14, Pin.IN, Pin.PULL_UP)
 button_y = Pin(15, Pin.IN, Pin.PULL_UP)
 
-#TODO: turn all these into one function call, and then use button X to change themes
-standard_theme_hue = [323, 330, 340, 350, 0, 17, 28, 40, 60, 98, 150, 170, 180, 190, 198, 225, 215, 242, 242, 270, 290, 305, 312]
-standard_theme_sat = [0.81, 0.84, 0.85, 0.9, 0.73, 0.80, 0.68, 0.65, 0.55, 0.85, 0.93, 0.95, 0.40, 0.71, 0.65, 0.40, 0.71, 0.53, 0.89, 0.73, 0.89, 0.89, 0.89]
-#another_theme_hue
-#another_theme_sat
+# set up palettes
+#TODO: do we actually need both of these?
+palette_list = ['standard', 'chill']
+current_palette = 0
 
-digits = [None] * 11
+standard_palette_hue = [323, 330, 340, 350, 0, 10, 15, 26, 38, 60, 98, 150, 170, 180, 190, 198, 225, 215, 242, 242, 270, 290, 305, 312]
+standard_palette_sat = [0.81, 0.84, 0.85, 0.9, 0.83, 0.89 , 0.68, 0.9, 0.65, 0.55, 0.85, 0.93, 0.95, 0.40, 0.71, 0.65, 0.40, 0.71, 0.53, 0.89, 0.73, 0.89, 0.89, 0.89]
+
+chill_palette_hue = []
+chill_palette_sat = [1 for k in range(24)]
+#TODO: make the chill palette's colors more dilberately?
+for i in range(160, 256, 4):
+    chill_palette_hue += [i]
+        
+theme_hue = standard_palette_hue
+theme_sat = standard_palette_sat
 
 # position coordinates from top left of the number
+digits = [None] * 11
 digits[0] = [
 [0,0],[1,0],[2,0],
 [0,1],      [2,1],
@@ -161,19 +178,67 @@ digits[10] = [
 [0,6],      [2,6]
 ]
 
-def Digit(num, offset):
+def get_palette_pos(val):
+    if val > 23:
+        val = val%24
+        return val
+    else:
+        return val
+    
+def set_palette(name):
+    global theme_hue
+    global theme_sat
+    
+    if name == 'standard':
+        theme_hue = standard_palette_hue
+        theme_sat = standard_palette_sat
+    elif name == 'chill':
+        theme_hue = chill_palette_hue
+        theme_sat = chill_palette_sat
+    
+    print('Change palette to: ' + str(name))
+    
+def set_current_quarter(val):
+    global currentQuarter
+    
+    currentQuarter = val
+    
+def HourFormat(hour):
+    # function to adjust global time to local time, considering user adjusted hours and DST
+    localhour = hour + hrdelta + dst
+    
+    #format properly if 12h clock being used
+    if hr12mode == True:
+        localhour = localhour%12
+        if localhour == 0:
+            localhour = 12
+            
+    # resets the hour if it goes above 23 during user time setting
+    if localhour > 23:
+        localhour = localhour%24
+    
+    firstDigit, secondDigit = split_digit(localhour)
+       
+    return [firstDigit, secondDigit, localhour]
+
+def split_digit(digit):
+    if digit < 10:
+        firstDigit = 0
+        secondDigit = digit
+    else:        
+        firstDigit = math.floor(digit / 10)
+        secondDigit = math.floor(digit % 10)
+    return firstDigit, secondDigit
+
+        
+def Digit(num, offset, localhue=theme_hue[get_palette_pos(HourFormat(hr)[2])], localsat=theme_sat[get_palette_pos(HourFormat(hr)[2])]):
     for i in range(len(num)):
         x = num[i][0]        
         y = num[i][1]
         
-        #localhue = ColorShift(hours[hr][0], 10)
-        localhue = standard_theme_hue[hr]
-        localsat = standard_theme_sat[hr]
         localvalue = VB(255, 50)
-        
-        #shift position over by an offset number and draw pixels          
-        picounicorn.set_pixel(x + offset, y, *Rainbow(localhue, localsat, localvalue))
-
+        r, g, b = Rainbow(localhue, localsat, localvalue)        
+        picounicorn.set_pixel(x + offset, y, r, g, b)
             
 def ClearDisplay():
     for x in range(w):
@@ -181,15 +246,15 @@ def ClearDisplay():
             #draws "black" pixels
             picounicorn.set_pixel(x, y, 0, 0, 0)
             
-def Pillar(width, offset, localvalue, height=h, localhue=standard_theme_hue[hr], localsat=standard_theme_sat[hr]):
+def Pillar(width, offset, localvalue, height=h, localhue=theme_hue[get_palette_pos(HourFormat(hr)[2])], localsat=theme_sat[get_palette_pos(HourFormat(hr)[2])]):
+    r, g, b = Rainbow(localhue, localsat, localvalue)
     for x in range(width):
         for y in range(height):
-            picounicorn.set_pixel(x + offset, y, *Rainbow(localhue, localsat, localvalue))
+            picounicorn.set_pixel(x + offset, y, r, g, b)
 
 def Rainbow(hue, sat, val):
-    #hu is 0-360, sa is 0.0-1.0 and va is 0-255
-    global shift
-    hue = (hue + shift)%359
+    # incoming values: hue is 0-360, sat is 0.0-1.0 and val is 0-255
+    hue = (hue)%359
     hue = (hue)/360
     sat = sat
     
@@ -198,26 +263,22 @@ def Rainbow(hue, sat, val):
     r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, sat, val)]
     return (r, g, b)
 
-def TogglePalette():
-    print('toggle palette')
-
-def ColorShift(color, delta=10):
-    adj = color + delta
-    return adj
-
 def Brightness(default=False):
+    # function for changing the brightness by 1 level
     global brt
     
     if default == True or brt < 0.4:
-        brt = round(1.0, 1)
+        brt = 1.0
     else:
-        brt = brt - round(0.2, 1)
+        brt = brt - 0.2
+        
+    print('Change brightness to : ' + str(brt))    
 
     Reset()
     sleep(0.1)
 
 def VB(value, minimum=0):
-    # variable brightness
+    # function to handle variable brightness across anything drawn
     value = value * brt
 
     if value < minimum:
@@ -226,34 +287,25 @@ def VB(value, minimum=0):
     return int(value)
 
 def Pulse():
-    global hr
+    forwardhue = theme_hue[get_palette_pos(HourFormat(hr)[2]+1)]
+    forwardsat = theme_sat[get_palette_pos(HourFormat(hr)[2]+1)]
     
-    #TODO: maybe replace this with a function, it's used three times kind of
-    if hr == 23:
-        forwardhue = standard_theme_hue[0]
-        forwardsat = standard_theme_sat[0]
-    else:
-        forwardhue = standard_theme_hue[hr+1]
-        forwardsat = standard_theme_sat[hr+1]
-    
-    
-    #TODO: continue working on this to optimize
-    if currentQuarter == 0:
-        localOffset = (currentSecond%6)+quarterOffsets[1]
-    elif currentQuarter == 1:
-        temp = (currentSecond%6)
-        if temp == 0 or temp == 1:
-            localOffset = (currentSecond%6)+quarterOffsets[0]
-        else:
-            localOffset = (currentSecond%6)+quarterOffsets[1] #this
+    if currentQuarter == 1:
+        localOffset = (currentSecond%6)+quarterOffsets[2]
     elif currentQuarter == 2:
         temp = (currentSecond%6)
-        if temp == 4 or temp == 5:
-            localOffset = (currentSecond%6)-4+quarterOffsets[3]
+        if temp == 0 or temp == 1:
+            localOffset = (currentSecond%6)+quarterOffsets[1]
         else:
-            localOffset = (currentSecond%6)+quarterOffsets[0]
+            localOffset = (currentSecond%6)+quarterOffsets[2]
     elif currentQuarter == 3:
-        localOffset = (currentSecond%6)+quarterOffsets[0]
+        temp = (currentSecond%6)
+        if temp == 4 or temp == 5:
+            localOffset = (currentSecond%6)-4+quarterOffsets[4]
+        else:
+            localOffset = (currentSecond%6)+quarterOffsets[1]
+    elif currentQuarter == 4:
+        localOffset = (currentSecond%6)+quarterOffsets[1]
         
     for i in range(VB(50,25), VB(210, 120), 25):
         Pillar(1, localOffset, i, localhue=forwardhue, localsat=forwardsat)
@@ -263,37 +315,27 @@ def Pulse():
         sleep(0.05)
 
 def DisplayQuarters():
-    global currentQuarter
-    global hr
     flux = False
     
-    #TODO: replace
-    if hr == 23:
-        forwardhue = standard_theme_hue[0]
-        forwardsat = standard_theme_sat[0]
-    else:
-        forwardhue = standard_theme_hue[hr+1]
-        forwardsat = standard_theme_sat[hr+1]
+    # set the quarters hue to the upcoming hour's hue, for some visual contrast
+    forwardhue = theme_hue[get_palette_pos(HourFormat(hr)[2]+1)]
+    forwardsat = theme_sat[get_palette_pos(HourFormat(hr)[2]+1)]
     
     # check which quarter we are in
     if currentMinute <= 13 or currentMinute == 59:
-        print("first quarter")
-        currentQuarter = 0
+        set_current_quarter(1)
     elif currentMinute >= 14 and currentMinute <= 28:
-        print("second quarter")
-        currentQuarter = 1
+        set_current_quarter(2)
     elif currentMinute >= 29 and currentMinute <= 43:
-        print("third quarter")
-        currentQuarter = 2
+        set_current_quarter(3)
     elif currentMinute >= 44 and currentMinute <= 58:
-        print("fourth quarter")
-        currentQuarter = 3
+        set_current_quarter(4)
         
     if currentMinute == 14 or currentMinute == 29 or currentMinute == 44 or currentMinute == 59:
         flux = True
     
     # draw each of the 4 quarters
-    for i in range(4):
+    for i in range(1,5):
         
         # for the current quarter
         if i == currentQuarter:
@@ -351,20 +393,39 @@ def DisplayQuarters():
             Pillar(2, quarterOffsets[i], VB(50, 25), localhue=forwardhue, localsat=forwardsat)
         
 def DisplayHours():
-    # display main digits
-    Digit(digits[HourFormat()[0]], 0)
-    Digit(digits[HourFormat()[1]], 4)
+    # function to display the main digits
+    Digit(digits[HourFormat(hr)[0]], 0, localhue=theme_hue[get_palette_pos(HourFormat(hr)[2])], localsat=theme_sat[get_palette_pos(HourFormat(hr)[2])])
+    Digit(digits[HourFormat(hr)[1]], 4, localhue=theme_hue[get_palette_pos(HourFormat(hr)[2])], localsat=theme_sat[get_palette_pos(HourFormat(hr)[2])])
     
-def HourDelta():
+def HourDelta(delta=1):
+    # function for changing the hour by a set amount
     global hrdelta
-    hrdelta = hrdelta + 1
+    hrdelta = hrdelta + delta
     if hrdelta > 23:
         hrdelta = 0
         
 def ToggleHR12Mode():
+    # function for toggling 12-hour clock on or off
     global hr12mode
     hr12mode = not hr12mode
-            
+    print('Toggle 12/24h mode')
+
+def dst_adjust():
+    # function to determine if it's the date and time to change the clock automatically
+    
+    global dst
+    
+    #TODO: test this properly
+    if currentMonth >= 11 or currentMonth <= 3:
+        if currentMonth == 11 and currentDate >= 7:
+            dst = 0
+        elif currentMonth == 3 and currentDate <= 14:
+            dst = 0      
+        else:
+            dst = 1
+    else:
+        dst = 1
+                            
 def HourTransition():
     # animation when changing hours
     for i in range(w/2):
@@ -384,33 +445,6 @@ def HourTransition():
         Pillar(1, (math.ceil(w/2)+1) + (i-1), 0)
         Pillar(1, (math.floor(w/2)-1) - (i), 0)
         sleep(1/w)
-
-def HourFormat():
-    global hr
-    global hrdelta
-    
-    localhour = hr + hrdelta
-    
-    #format properly if 12h clock being used
-    if hr12mode == True:
-        localhour = localhour%12
-        if localhour == 0:
-            localhour = 12
-            
-    # resets the hour if it goes above 23 during user time setting
-    if localhour > 23:
-        localhour = localhour%24
-    
-    # formats the two-digit hours into two single-digit numbers
-    if localhour < 10:
-        firstDigit = 0
-        secondDigit = localhour
-    else:        
-        firstDigit = math.floor(localhour / 10)
-        secondDigit = math.floor(localhour % 10)
-       
-    return [firstDigit, secondDigit]
-        
 
 def hsv_to_rgb(h, s, v):
     if s == 0.0:
@@ -441,20 +475,24 @@ def callback_button_a(pin):
     if d == None:
         return
     elif not d:
-        #TODO: try threading Event before giving up on optimizing this
         exitLoop = False
         youwin = False
         sleep(0.5)
         while not exitLoop:
-            for i in range(16):
-                
-                #TODO: use Rainbow() here?
-                hue = (standard_theme_hue[i])/360
-                r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 0.8)]
+            print('Toggle disco mode')
+            for i in range(32):
+                if i < 16:
+                    pos = i
+                    hue = theme_hue[i]
+                else:
+                    pos = 31 - i
+                    hue = theme_hue[i - 16]
+ 
+                r, g, b = Rainbow(hue, 1.0, 220)
                 
                 for x in range(w):
                     for y in range(h):
-                        picounicorn.set_pixel(i, y, r, g, b)
+                        picounicorn.set_pixel(pos, y, r, g, b)
                 sleep(0.01)
                 
                 if picounicorn.is_pressed(picounicorn.BUTTON_A):
@@ -463,47 +501,27 @@ def callback_button_a(pin):
                         youwin = True           
                     exitLoop = True
                     break
-                
-            for i in range(16):
-                
-                #TODO: use Rainbow() here?
-                hue = (standard_theme_hue[i])/360
-                r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 0.8)]
-                
-                for x in range(w):
-                    for y in range(h):
-                        picounicorn.set_pixel(15-i, y, r, g, b)
-                sleep(0.01)
-                
-                #TODO: move this and the above to a single for loop of 32 to optimize
-                if picounicorn.is_pressed(picounicorn.BUTTON_A):
-                    # if you stop the bar in the very center you win
-                    if i == 8:
-                        youwin = True
-                    exitLoop = True
-                    break
-                
+                sleep(1/1028)
             sleep(0.5)
+                
         if youwin == True:
 
             # celebrate, you win! ... an animation
-            for f in range(20): #flashing
-                
-                for a in range(16): #color changes
-                    #TODO: use Rainbow() here?
-                    hue = (f + (a*random.randint(4,8)))/360
-                    sat = random.randint(0,100)/100
-                    val = random.randint(0,10)/10
-                    r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, sat, val)]
+            for i in range(24): 
+                for pos in range(16):
+
+                    hue = random.randint(0,360)
+                    sat = random.randint(70,100)/100
+                    val = random.randint(50,255)
+                    r, g, b = Rainbow(hue, sat, val)
                     for x in range(w):
                         for y in range(h):
-                            picounicorn.set_pixel(a, random.randint(0,6), r, g, b)
-                    sleep(0.005)
-                sleep(0.04)
+                            picounicorn.set_pixel(pos, random.randint(0,6), r, g, b)
+                sleep(0.05)
         Reset()
 
 def callback_button_b(pin):
-    print('button B')
+    print('Button B')
     d = debounce(pin)
 
     if d == None:
@@ -521,7 +539,7 @@ def callback_button_b(pin):
                 ClearDisplay()
                 
                 # animation to show new hour format 
-                if hr12:
+                if hr12mode:
                     for f in range (6):
                         # draw blank first
                         Pillar(12, 0, 0)
@@ -543,25 +561,46 @@ def callback_button_b(pin):
                         
                 Reset()
             
-            # otherwise, button changes brightness down by 1 level, of 5, then loops around
+            # if button is not held, changes brightness down by 0.2, then loops around to 1.0 brightness
             else:
                 Brightness()
                 break
             
         
 def callback_button_x(pin):
-    print('button X')
+    print('Button X')
     d = debounce(pin)
 
     if d == None:
         return
     elif not d:
-        global shift
-        #shift = ColorShift(shift, 20)
+        global current_palette
+        current_palette = (current_palette + 1) % len(palette_list)
+        set_palette(palette_list[current_palette])
+        
+        # change to new palette, then display palette color range
+        for i in range(len(theme_hue)):
+            
+            ClearDisplay()
+            if hr12mode:
+                firstDigit, secondDigit = split_digit((i%12)+1)
+            else:
+                firstDigit, secondDigit = split_digit(i)
+
+            Digit(digits[firstDigit], 0, localhue=theme_hue[i], localsat=theme_sat[i])
+            Digit(digits[secondDigit], 4, localhue=theme_hue[i], localsat=theme_sat[i])
+            
+            for x in range(8, w):
+                for y in range(h):
+                    picounicorn.set_pixel(x, y, *Rainbow(theme_hue[get_palette_pos(i+1)], theme_sat[get_palette_pos(i+1)], brt*255))
+            sleep(0.6)
+        
         Reset()
+        Reset()
+        #TODO: something here to stop the nexttick Pulse from being the wrong color
 
 def callback_button_y(pin):
-    print('button Y')
+    print('Button Y')
     d = debounce(pin)
 
     if d == None:
@@ -571,8 +610,8 @@ def callback_button_y(pin):
         HourDelta()
         Reset()
     
-# i didn't write this function, but it helps prevent double button presses
 def debounce(pin):
+    # function to help prevent button presses from debouncing
     prev = None
     for _ in range(32):
         current_value = pin.value()
@@ -582,37 +621,30 @@ def debounce(pin):
     return prev
 
 def Reset():
+    # function to do a soft reset
     ClearDisplay()
     DisplayHours()
     DisplayQuarters()
 
+# interrupt requests
 button_a.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_a)
 button_b.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_b)
 button_x.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_x)
 button_y.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_y)
 
+# one-time setup
 Brightness(True)
+dst_adjust()
 HourTransition()
 Reset()
 
 while True:
     gc.collect()
     #print('Mem free: {} --- Mem allocated: {}'.format(gc.mem_free(), gc.mem_alloc()))
-    
-#     for i in range(len(standard_theme_hue)):
-#         localhue = standard_theme_hue[i]
-#         localsat = standard_theme_sat[i]
-#         
-#         print(i+1)
-#         
-#         for x in range(w):
-#             for y in range(h):
-#                 picounicorn.set_pixel(x, y, *Rainbow(localhue, localsat, 255))
-#         sleep(0.2)
 
     # runs once per second
     if currentSecond != rtc.get_seconds():
-        print(hr, currentMinute, currentSecond)
+        print(HourFormat(hr)[2], currentMinute, currentSecond)
         
         Pulse()
         currentSecond = rtc.get_seconds()
@@ -627,6 +659,9 @@ while True:
         if rtc.get_hours() != currentHour:
             currentHour = rtc.get_hours()
             
+            dst_adjust()
+            
+            #TODO: is this the right thing to do here...
             hr = hr + 1
             
             HourTransition()
