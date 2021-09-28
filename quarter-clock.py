3c9b6a04db80131c541ff68740008188714f89b6
Foster Douglas <foster@sandcastle.co>
2021-09-28 02:34:48 -0700

    Add files via upload

--- quarter-clock.py
+++ quarter-clock.py
@@ -1,6 +1,7 @@
 import picounicorn
 from utime import time, localtime, sleep
 import math
+import gc
 import rv3028_rtc
 from machine import Pin
 
@@ -10,9 +11,7 @@ w = picounicorn.get_width()
 h = picounicorn.get_height()
 
 # initalize the RV3028 RTC unit
-sda=machine.Pin(26)
-scl=machine.Pin(27)
-i2c=machine.I2C(1,sda=sda, scl=scl, freq=400000)
+i2c=machine.I2C(1,sda=Pin(26), scl=Pin(27), freq=100000)
 rtc=rv3028_rtc.RV3028(0x52, i2c, "LSM")
 
 currentSecond = rtc.get_seconds()
@@ -26,11 +25,13 @@ global currentQuarter
 global brt #brightness
 global hr #hour
 global shift #color shift
+global hr12 #24 or 12h clock
 
 currentQuarter = 0
-brt = 1
+brt = 0.8 #start brightness at one level down
 hr = currentHour
 shift = 0
+hr12 = False
 
 # using these instead of picounicorn's built in functions to be able to access IRQs
 button_a = Pin(12, Pin.IN, Pin.PULL_UP)
@@ -63,6 +64,7 @@ twenty = 20
 twentyone = 21
 twentytwo = 22
 twentythree = 23
+hh = 30 #for custom "h" letter
 
 # make a dictionary for digits 0 - 9, to map to the coordinates of pixels to draw each digit
 digits = {}
@@ -199,13 +201,29 @@ digits[nine] = [
             [2,6]
 ]
 
-def Digit(num, offset):
+digits[hh] = [
+[0,0],
+[0,1],
+[0,2],
+[0,3],[1,3],[2,3],
+[0,4],      [2,4],
+[0,5],      [2,5],
+[0,6],      [2,6]
+]
+
+def Digit(num, offset, erase=False):
     for i in range(len(num)):
         x = num[i][0]        
         y = num[i][1]
         
+        localhue = ColorShift(hours[hr], 10)
+        localbrightness = VB(255, 50)
+        
+        if erase:
+            localbrightness = 0
+        
         #shift position over by an offset number and draw pixels          
-        picounicorn.set_pixel(x + offset, y, *Rainbow(ColorShift(hours[hr], 10), VB(255, 50)))
+        picounicorn.set_pixel(x + offset, y, *Rainbow(localhue, localbrightness))
 
             
 def ClearDisplay():
@@ -214,21 +232,21 @@ def ClearDisplay():
             #draws "black" pixels
             picounicorn.set_pixel(x, y, 0, 0, 0)
             
-def Pillar(width, offset, brightness, height=h, hue=hours[hr], hueshift=0):
-    hue = hue + hueshift
+def Pillar(width, offset, localbrightness, height=h, localhue=hours[hr], hueshift=0):
+    localhue = localhue + hueshift
     for x in range(width):
         for y in range(height):
-            picounicorn.set_pixel(x + offset, y, *Rainbow(hue, brightness))
+            picounicorn.set_pixel(x + offset, y, *Rainbow(localhue, localbrightness))
 
-def Rainbow(hue, value):
-    hue = (hue + shift)%359
-    hue = (hue)/360
-    r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, value/255)]
+def Rainbow(localhue, localvalue):
+    global shift
+    localhue = (localhue + shift)%359
+    localhue = (localhue)/360
+    r, g, b = [int(c * 255) for c in hsv_to_rgb(localhue, 1.0, localvalue/255)]
     return (r, g, b)
 
 def ColorShift(color, delta=10):
     adj = color + delta
-    #print(adj)
     return adj
 
 def Brightness(default=False):
@@ -240,7 +258,6 @@ def Brightness(default=False):
         brt = brt - round(0.2, 1)
 
     Reset()
-    print("Brightness = " + str(brt))
     sleep(0.1)
 
 def VB(value, minimum=0):
@@ -252,46 +269,46 @@ def VB(value, minimum=0):
     return int(value)
 
 def Pulse():
-    if currentSecond != rtc.get_seconds():
-        global hr
-        
-        #TODO: maybe replace this with a function, it's used three times kind of
-        if hr == 23:
-            forwardhue = hours[0]
-        else:
-            forwardhue = hours[hr+1]
-        
-        
-        #TODO: continue working on this to optimize
-        if currentQuarter == 0:
-            localOffset = (currentSecond%6)+quarterOffsets[1]
-        elif currentQuarter == 1:
-            temp = (currentSecond%6)
-            if temp == 0 or temp == 1:
-                localOffset = (currentSecond%6)+quarterOffsets[0]
-            else:
-                localOffset = (currentSecond%6)+quarterOffsets[1] #this
-        elif currentQuarter == 2:
-            temp = (currentSecond%6)
-            if temp == 4 or temp == 5:
-                localOffset = (currentSecond%6)-4+quarterOffsets[3]
-            else:
-                localOffset = (currentSecond%6)+quarterOffsets[0]
-        elif currentQuarter == 3:
+    global hr
+    
+    #TODO: maybe replace this with a function, it's used three times kind of
+    if hr == 23:
+        forwardhue = hours[0]
+    else:
+        forwardhue = hours[hr+1]
+    
+    
+    #TODO: continue working on this to optimize
+    if currentQuarter == 0:
+        localOffset = (currentSecond%6)+quarterOffsets[1]
+    elif currentQuarter == 1:
+        temp = (currentSecond%6)
+        if temp == 0 or temp == 1:
             localOffset = (currentSecond%6)+quarterOffsets[0]
-            
-        for i in range(VB(50,25), VB(210, 120), 25):
-            Pillar(1, localOffset, i, hue=forwardhue, hueshift=20)
-            sleep(0.05)
-        for i in reversed(range(VB(50,25), VB(210, 120), 25)):
-            Pillar(1, localOffset, i, hue=forwardhue, hueshift=20)
-            sleep(0.05)
+        else:
+            localOffset = (currentSecond%6)+quarterOffsets[1] #this
+    elif currentQuarter == 2:
+        temp = (currentSecond%6)
+        if temp == 4 or temp == 5:
+            localOffset = (currentSecond%6)-4+quarterOffsets[3]
+        else:
+            localOffset = (currentSecond%6)+quarterOffsets[0]
+    elif currentQuarter == 3:
+        localOffset = (currentSecond%6)+quarterOffsets[0]
+        
+    for i in range(VB(50,25), VB(210, 120), 25):
+        Pillar(1, localOffset, i, localhue=forwardhue, hueshift=20)
+        sleep(0.05)
+    for i in reversed(range(VB(50,25), VB(210, 120), 25)):
+        Pillar(1, localOffset, i, localhue=forwardhue, hueshift=20)
+        sleep(0.05)
 
 def DisplayQuarters():
     global currentQuarter
     global hr
     flux = False
     
+    #TODO: replace
     if hr == 23:
         forwardhue = hours[0]
     else:
@@ -314,10 +331,9 @@ def DisplayQuarters():
     if currentMinute == 14 or currentMinute == 29 or currentMinute == 44 or currentMinute == 59:
         flux = True
     
+    # draw each of the 4 quarters
     for i in range(4):
         
-        
-
         # for the current quarter
         if i == currentQuarter:
             
@@ -325,53 +341,53 @@ def DisplayQuarters():
                 # animation for blink/fade each minute
                 print('minute blink')
                 for desc in reversed(range(VB(34,0), VB(204, 100), 17)):  
-                    Pillar(2, quarterOffsets[i], desc, hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[i], desc, localhue=forwardhue, hueshift=20)
                     sleep(0.02)
                 for asc in range(VB(34,0), VB(204, 100), 17):  
-                    Pillar(2, quarterOffsets[i], asc, hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[i], asc, localhue=forwardhue, hueshift=20)
                     sleep(0.02)
                     
-                Pillar(2, quarterOffsets[i], VB(240, 80), hue=forwardhue, hueshift=20)
+                Pillar(2, quarterOffsets[i], VB(240, 80), localhue=forwardhue, hueshift=20)
                 
             # animate for each quarter change
             if flux == True:
                 
                 if i > 0:
-                    Pillar(2, quarterOffsets[i - 1], VB(240, 80), hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[i - 1], VB(240, 80), localhue=forwardhue, hueshift=20)
                     
                     # animate off previous quarter
                     print('previous quarter off')
                     for off in range(h):
-                        Pillar(2, quarterOffsets[i - 1], 0, off + 1, hue=forwardhue, hueshift=20)
+                        Pillar(2, quarterOffsets[i - 1], 0, off + 1, localhue=forwardhue, hueshift=20)
                         sleep(0.03)
                     # replace the emptiness with faded quarter
-                    Pillar(2, quarterOffsets[i - 1], VB(50, 25), hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[i - 1], VB(50, 25), localhue=forwardhue, hueshift=20)
                     
                      #animate next quarter over
                     print('next quarter on')
                     for on in range(h):
-                        Pillar(2, quarterOffsets[i], VB(240, 80), on + 1, hue=forwardhue, hueshift=20)
+                        Pillar(2, quarterOffsets[i], VB(240, 80), on + 1, localhue=forwardhue, hueshift=20)
                         sleep(0.03)
-                    Pillar(2, quarterOffsets[i], VB(240, 80), hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[i], VB(240, 80), localhue=forwardhue, hueshift=20)
                 else:
                     # animate off previous quarter
                     print('previous quarter off')
                     for off in range(h):
-                        Pillar(2, quarterOffsets[3], 0, off + 1, hue=forwardhue, hueshift=20)
+                        Pillar(2, quarterOffsets[3], 0, off + 1, localhue=forwardhue, hueshift=20)
                         sleep(0.03)
                     # replace the emptiness with faded quarter
-                    Pillar(2, quarterOffsets[3], VB(50, 25), hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[3], VB(50, 25), localhue=forwardhue, hueshift=20)
                     
                     # otherwise, animate the first quarter   
                     print('next quarter on')
                     for on in range(h):
-                        Pillar(2, quarterOffsets[0], VB(240, 80), on + 1, hue=forwardhue, hueshift=20)
+                        Pillar(2, quarterOffsets[0], VB(240, 80), on + 1, localhue=forwardhue, hueshift=20)
                         sleep(0.03)
-                    Pillar(2, quarterOffsets[0], VB(240, 80), hue=forwardhue, hueshift=20)
+                    Pillar(2, quarterOffsets[0], VB(240, 80), localhue=forwardhue, hueshift=20)
         
         # for the other 3 faded quarters
         else:
-            Pillar(2, quarterOffsets[i], VB(50, 25), hue=forwardhue, hueshift=20)
+            Pillar(2, quarterOffsets[i], VB(50, 25), localhue=forwardhue, hueshift=20)
         
 def DisplayHours():
     # display main digits
@@ -402,16 +418,25 @@ def HourFormat():
     global hr
     
     # resets the hour if it goes above 23 during user time setting
+    # TODO: replace
     if hr > 23:
         hr = 0
     
+    localhour = hr
+    
+    #format properly if 12h clock being used
+    if hr12 == True:
+        localhour = localhour%12
+        if localhour == 0:
+            localhour = 12
+    
     # formats the two-digit hours into two single-digit numbers
-    if hr < 10:
+    if localhour < 10:
         firstDigit = 0
-        secondDigit = hr
+        secondDigit = localhour
     else:        
-        firstDigit = math.floor(hr / 10)
-        secondDigit = math.floor(hr % 10)
+        firstDigit = math.floor(localhour / 10)
+        secondDigit = math.floor(localhour % 10)
        
     return [firstDigit, secondDigit]
         
@@ -450,6 +475,8 @@ def callback_button_a(pin):
         sleep(0.5)
         while not exitLoop:
             for i in range(16):
+                
+                #TODO: use Rainbow() here?
                 hue = (hours[i])/360
                 r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 0.8)]
                 
@@ -463,6 +490,8 @@ def callback_button_a(pin):
                     break
                 
             for i in range(16):
+                
+                #TODO: use Rainbow() here?
                 hue = (hours[i])/360
                 r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 0.8)]
                 
@@ -485,7 +514,46 @@ def callback_button_b(pin):
     if d == None:
         return
     elif not d:
-        Brightness()
+        # check if button held
+        for t in range (31):
+            if picounicorn.is_pressed(picounicorn.BUTTON_B):
+               sleep(0.1)
+               
+               # if held for 3 sec, toggle time between 12/24h
+               if t == 30:
+                global hr12
+                hr12 = not hr12
+                
+                ClearDisplay()
+                
+                # animation to show new hour format 
+                if hr12:
+                    for f in range (6):
+                        # draw blank first
+                        Pillar(12, 0, 0)
+                        sleep(0.1)
+                        Digit(digits[1], 0)
+                        Digit(digits[2], 4)
+                        Digit(digits[hh], 8)
+                        sleep(0.2)
+                        
+                else:
+                    for f in range (6):
+                        # draw blank first
+                        Pillar(12, 0, 0)
+                        sleep(0.1)
+                        Digit(digits[2], 0)
+                        Digit(digits[4], 4)
+                        Digit(digits[hh], 8)
+                        sleep(0.2)
+                        
+                Reset()
+            
+            # otherwise, change brightness
+            else:
+                Brightness()
+                break
+            
         
 def callback_button_x(pin):
     print('button X')
@@ -510,7 +578,7 @@ def callback_button_y(pin):
         hr = hr + 1    
         Reset()
     
-# i didn't write this function
+# i didn't write this function, but it helps prevent double button presses
 def debounce(pin):
     prev = None
     for _ in range(32):
@@ -522,34 +590,28 @@ def debounce(pin):
 
 def Reset():
     ClearDisplay()
-
     DisplayHours()
     DisplayQuarters()
 
-def Setup():
-    # on startup, clear any past display memory
-    ClearDisplay()
-    
-    HourTransition()
-    DisplayHours()
-    DisplayQuarters()
+button_a.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_a)
+button_b.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_b)
+button_x.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_x)
+button_y.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_y)
 
 Brightness(True)
-Setup()
+HourTransition()
+Reset()
+
 
 while True:
-    
-    button_a.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_a)
-    button_b.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_b)
-    button_x.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_x)
-    button_y.irq(trigger=Pin.IRQ_FALLING, handler=callback_button_y)
+    gc.collect()
+    #print('Mem free: {} --- Mem allocated: {}'.format(gc.mem_free(), gc.mem_alloc()))
 
-    # only run once per second
+    # runs once per second
     if currentSecond != rtc.get_seconds():
         print(hr, currentMinute, currentSecond)
         
         Pulse()
-        
         currentSecond = rtc.get_seconds()
                 
         # check minute        
@@ -562,8 +624,14 @@ while True:
         if rtc.get_hours() != currentHour:
             currentHour = rtc.get_hours()
             
+            #TODO: replace
             hr = hr + 1
+            
             HourTransition()
             DisplayQuarters()
         
         DisplayHours()
+
+        gc.collect()
+    else:
+        pass
